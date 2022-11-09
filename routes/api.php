9f57e77aa70a88295935b0d5479d6c75364c8545
ssadrian <113125604+ssadrian@users.noreply.github.com>
2022-11-09 23:31:32 +0100

    Add auth middleware

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,14 @@ use Illuminate\Support\Facades\Route;
 Route::post("register", [AuthController::class, "register"]);
 Route::post("login", [AuthController::class, "login"]);
 
-Route::get('product', [ProductController::class, 'get']);
-Route::post('product', [ProductController::class, 'post']);
-Route::put('product', [ProductController::class, 'put']);
-Route::delete('product', [ProductController::class, 'delete']);
+Route::group(["middleware" => "auth:sanctum"], function () {
+    Route::get('product', [ProductController::class, 'get']);
+    Route::post('product', [ProductController::class, 'post']);
+    Route::put('product', [ProductController::class, 'put']);
+    Route::delete('product', [ProductController::class, 'delete']);
 
-Route::get('category', [CategoryController::class, 'get']);
-Route::post('category', [CategoryController::class, 'post']);
-Route::put('category', [CategoryController::class, 'put']);
-Route::delete('category', [CategoryController::class, 'delete']);
-
-Route::middleware("auth:sanctum")
-    ->get("/user", fn(Request $request) => $request->user());
+    Route::get('category', [CategoryController::class, 'get']);
+    Route::post('category', [CategoryController::class, 'post']);
+    Route::put('category', [CategoryController::class, 'put']);
+    Route::delete('category', [CategoryController::class, 'delete']);
+});
